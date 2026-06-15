4ef05c201b3daf9efe99540d7267a6409dbacbaa
dev <martinlestoppeur@gmail.com>
2026-06-15 10:57:50 +0200

    modif app.js pour test sur railway
    configuration environnement production Railway

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const path = require('path');
 const { connect } = require('./db/mongo');
 
-dotenv.config({ path: './env/.env.dev' });
+if (process.env.NODE_ENV === 'production') {
+} else {
+  dotenv.config({ path: './env/.env.dev' });
+}
 
 connect();
 
 const app = express();
 
-const path = require('path');
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const session = require('express-session');
+const methodOverride = require('method-override');
 
 app.use(session({
-  secret: process.env.SECRET_KEY,
+  secret: process.env.SECRET_KEY || 'secret_fallback',
   resave: false,
   saveUninitialized: false,
   cookie: { secure: false }
@@ -24,6 +28,7 @@ app.use(session({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 app.use('/users', require('./routes/users'));
 app.use('/', require('./routes/views'));
