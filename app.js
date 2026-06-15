adf207fe0523653e8f82aeeb03fe70d404810810
dev <martinlestoppeur@gmail.com>
2026-06-15 09:38:25 +0200

    app.js maj
     configuration Express et montage des routes

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const dotenv = require('dotenv');
+const { connect } = require('./db/mongo');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+dotenv.config({ path: './env/.env.dev' });
 
-var app = express();
+connect();
 
-app.use(logger('dev'));
+const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/catways', require('./routes/catways'));
+app.use('/api/catways/:id/reservations', require('./routes/reservations'));
+
+// 404
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route introuvable' });
+});
 
 module.exports = app;
