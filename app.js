c797a56b14cc7dfdb2069c67f7aa88430a053f51
dev <martinlestoppeur@gmail.com>
2026-06-15 10:17:33 +0200

    modif routes

--- app.js
+++ app.js
@@ -11,12 +11,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Routes
-app.use('/api/users', require('./routes/users'));
+app.use('/users', require('./routes/users'));
 app.use('/api/catways', require('./routes/catways'));
 app.use('/api/catways/:id/reservations', require('./routes/reservations'));
 
-// 404
 app.use((req, res) => {
   res.status(404).json({ message: 'Route introuvable' });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const service = require('../services/users');
+const { protect } = require('../middlewares/jwt');
 
-router.post('/register', service.register);
 router.post('/login', service.login);
+router.get('/logout', service.logout);
+
+router.get('/', protect, service.getAll);
+router.get('/:email', protect, service.getByEmail);
+router.post('/', protect, service.register);
+router.put('/:email', protect, service.update);
+router.delete('/:email', protect, service.remove);
 
 module.exports = router;
